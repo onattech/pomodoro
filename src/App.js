122529b47b516f37f309ed423a7a095161d219ca
lokutech <onatim@protonmail.com>
2020-01-05 00:04:23 +0300

    %80 done with the basic layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
-function App() {
-  return (
-    <div>
-      Hello
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    break: 5,
+    session: 25
+  }
+  render() {
+    return (
+      <div id='wrapper'>
+        <h1 id='title'>Pomodoro Clock</h1>
+        <div id='break-label'>
+          <h2>Break Length</h2>
+          <i id='break-increment' className='fas fa-arrow-up'></i>
+          <span id='break-length'>{this.state.break}</span>
+          <i id='break-decrement' className='fas fa-arrow-down'></i>
+        </div>
+        <div id='session-label'>
+          <h2>Session Length</h2>
+          <i id='session-increment' className='fas fa-arrow-up'></i>
+          <span id='session-length'>{this.state.session}</span>
+          <i id='session-decrement' className='fas fa-arrow-down'></i>
+        </div>
+        <div id="timer-label">
+          <h2>Session</h2>
+          <p id="time-left">25:00</p>
+        </div>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
